b19dbdeb9ac522c1ea09fdd7b41fdd3d64820008
Sulliivan <64897675+Sulliivan@users.noreply.github.com>
2020-10-28 12:10:32 +0100

    get single post

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using entity.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace entity.Controllers
@@ -7,6 +10,29 @@ namespace entity.Controllers
     [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        
+      //  private static User student = new User();
+      private static List<User> students = new List<User> 
+      {
+          new User(),
+          new User {Id = 1, Name = "Christopher"}
+      };
+
+    [HttpGet("AllStudents")]
+        public IActionResult Get()
+        {
+            return Ok(students);
+        }
+    [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(students.FirstOrDefault(c => c.Id == id));
+        }
+    [HttpPost]
+        public IActionResult AddStudents(User NewUser)
+        {
+            students.Add(NewUser);
+            return Ok(students);
+        }
+    
     }
 }
