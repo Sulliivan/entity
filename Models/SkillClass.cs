9a638586cc871ad8d0362ba9fa55407bf22a89b6
Sulliivan <64897675+Sulliivan@users.noreply.github.com>
2020-10-28 10:05:00 +0100

    tuto entity

---
+++ Models/SkillClass.cs
@@ -0,0 +1,9 @@
+namespace entity.Models
+{
+    public enum SkillClass
+    {
+        front = 1,
+        back = 2,
+        fullstack = 3,
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace entity.Models
+{
+    public class User
+    {
+        public int Id {get; set;}
+        public string Name {get; set;} = "Kevin";
+        public int Age {get; set;}=23;
+        public SkillClass Skill { get; set; } = SkillClass.fullstack;
+    }
+}
